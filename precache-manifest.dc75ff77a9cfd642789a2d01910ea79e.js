fc4762503d77c06abb306d0447e10e58d7d0ee43
irina08 <irina_ger@yahoo.com>
2019-11-09 21:36:16 -0800

    Updates

---
+++ precache-manifest.dc75ff77a9cfd642789a2d01910ea79e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98c60ee1a5c5aa11ad1547e0412b5f4",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "efa8f979eb0b45324835",
+    "url": "/react_app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "eb0239bc5d529b9fa561",
+    "url": "/react_app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "efa8f979eb0b45324835",
+    "url": "/react_app/static/js/2.7d878c5d.chunk.js"
+  },
+  {
+    "revision": "eb0239bc5d529b9fa561",
+    "url": "/react_app/static/js/main.6c11e472.chunk.js"
+  },
+  {
+    "revision": "5fbc95e35e06472b7b2f",
+    "url": "/react_app/static/js/runtime-main.364e78a2.js"
+  }
+]);
