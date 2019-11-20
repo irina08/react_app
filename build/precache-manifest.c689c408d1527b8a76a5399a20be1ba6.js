c27b48d1625690cfc3f555e0b7007138cb6bccdd
irina08 <38026611+irina08@users.noreply.github.com>
2019-11-19 22:22:19 -0800

    Add files via upload

---
+++ build/precache-manifest.c689c408d1527b8a76a5399a20be1ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6120be0e5c2edfddf3036ff6f9faec",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "efa8f979eb0b45324835",
+    "url": "/react_app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "06a0a1b51a11a8fa608b",
+    "url": "/react_app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "efa8f979eb0b45324835",
+    "url": "/react_app/static/js/2.7d878c5d.chunk.js"
+  },
+  {
+    "revision": "06a0a1b51a11a8fa608b",
+    "url": "/react_app/static/js/main.7d1d2bd1.chunk.js"
+  },
+  {
+    "revision": "5fbc95e35e06472b7b2f",
+    "url": "/react_app/static/js/runtime-main.364e78a2.js"
+  }
+]);
